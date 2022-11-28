c1a5e71667d0c3bb013ae590ec8afb1e0e7fc6b3
Yazid.Kejar <yazid.kejar@gmail.com>
2022-11-28 11:22:28 +0700

    Portfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* <<<<<<<<<<<<<<<<<<<<<<< typing animation >>>>>>>>>>>>>>>>>>>>>>>>> */
+var typed = new Typed(".typing",{
+  strings: ["","Web Designer", "Web Developer", "Frontend Developer", "Backend Developer"],
+  typeSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
